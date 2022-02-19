b436d76e01338b5b34054fba73dc05ed90227da7
Sadykov Daniil <sad1dani@mail.ru>
2022-02-19 17:02:29 +0300

    Fix long time send

---
+++ Teleg/Buttons.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Teleg
+{
+    class Buttons
+    {
+        public string Text { get; set; }
+        public Method Action { get; set; }
+        public bool Chosen { get; set; }
+        public void Add(string textOnButton, Method actionButton, bool chooseButton)
+        {
+            Text = textOnButton;
+            Action = actionButton;
+            Chosen = chooseButton;
+        }
+    }
+}
